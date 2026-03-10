4cef53fbef2b674c8e54dc1e95de5ec53ed9e5ca
Noel Tran <dinhanhduy.tran@gmail.com>
2026-03-11 01:56:20 +1100

    account + activity tab

--- app/(tabs)/activity.tsx
+++ app/(tabs)/activity.tsx
@@ -1,11 +1,28 @@
 import Container from "@/components/Container";
-import React from "react";
-import { Text } from "react-native";
+import Title from "@/components/Title";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function Activity() {
+import React from "react";
+import { View } from "react-native";
+import tw from "twrnc";
+
+const Activity = () => {
   return (
     <Container>
-      <Text>activity</Text>
+      <Title>Activity</Title>
+      <View style={tw`py-3 flex-row items-center justify-between`}>
+        <View
+          style={tw`w-10 h-10 rounded-full bg-black items-center justify-center`}
+        >
+          <Ionicons name="options" size={20} color={"white"} />
+        </View>
+        <Title className="text-xl">Past</Title>
+      </View>
+      <Title className="text-lg font-normal mt-3">
+        You don't have any recent activities
+      </Title>
     </Container>
   );
-}
+};
+
+export default Activity;
